3275245a69f2c2b6ea35c002256d459368792ead
Pranav Sharma <ps5840432@gmail.com>
2025-03-25 14:25:34 +0530

    first commit

---
+++ client-testimonial/src/App.jsx
@@ -0,0 +1,15 @@
+import TestimonealCard from "./components/TestimonealCard"
+
+
+function App() {
+
+
+  return (
+    <>
+      <TestimonealCard Name="Emily watson"  Reviewtext="your company product is amazing"/>
+      <TestimonealCard Name="Pranav Sharma"  Reviewtext="your company product is amazing"/>
+    </>
+  )
+}
+
+export default App
---
+++ client-testimonial/src/components/TestimonealCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestimonealCard = ({Name,Reviewtext}) => {
+  return (
+    <div>{Name} {Reviewtext}</div>
+  )
+}
+
+export default TestimonealCard
